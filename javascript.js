d28cc82a37a648210c7e4ad0982b2968263a742f
Morgan Hamet <morganhamet@gmail.com>
2022-05-28 16:39:34 -0700

    Add multiple colors for squares

--- javascript.js
+++ javascript.js
@@ -4,8 +4,12 @@ for (let i = 0; i < 16; i++) {
     const square = document.createElement("div");
     square.classList.add('square');
     grid.appendChild(square);
-    square.addEventListener('mouseenter', () => {
-        square.style.backgroundColor = "purple";
+    square.addEventListener('mouseover', () => {
+        let colors = ["red", "blue", "orange"];
+        let colorChoice = Math.floor(Math.random() * 3);
+        let colorString = String(colors[colorChoice]);
+        square.style.backgroundColor = colorString;;
     });
 }
+
 
