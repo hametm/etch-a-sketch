a6875e9b7d3e57937644ed8161f6d646ad29050e
Morgan Hamet <morganhamet@gmail.com>
2022-05-30 18:34:23 -0700

    Add color buttons

--- javascript.js
+++ javascript.js
@@ -1,34 +1,69 @@
 const grid = document.querySelector("#grid");
-const sizeButton = document.querySelector("#sizeChoiceBtn");
+const monochromeButton = document.querySelector("#monochromeButton");
+const rainbowButton = document.querySelector("#rainbowButton");
+const sizeChoice = document.querySelector("#sizeChoice");
+let slider = document.querySelector("#slider");
 
-let userChoice;
-let squareSize;
+let userChoice = 10;
+let squareSize = 50;
 
-sizeButton.addEventListener('click', () => {
-    userChoice = prompt("How many squares per row?");
+console.log(slider.value);
+
+rainbowButton.addEventListener('click', () => {
+    grid.innerHTML = '';
     squareSize = 500 / userChoice;
-    console.log(squareSize);
-    console.log(userChoice ** 2);
+    createRainbowGrid();
 });
 
-sizeButton.addEventListener('click', () => {
+monochromeButton.addEventListener('click', () => {
+    grid.innerHTML = '';
+    squareSize = 500 / userChoice;
+    createMonochromeGrid();
+})
+
+// This needs work
+slider.addEventListener('onmousemove', () => {
+    userChoice = slider.value;
+    console.log(slider.value);
+});
+
+function createMonochromeGrid() {
     for (let i = 0; i < userChoice ** 2; i++) {
         const square = document.createElement("div");
-        square.classList.add('square');
-        square.style.height = `${squareSize}px`;
-        square.style.width = `${squareSize}px`;
-    
-        grid.appendChild(square);
+        setSquareStyle(square);
         square.addEventListener('mouseover', () => {
-            let colors = ["red", "blue", "orange", "purple", "green", "yellow"];
-            currentIndex = colors.indexOf(square.style.backgroundColor);
-            colorsMinusCurrent = colors.splice(currentIndex, 1);
-            let colorChoice = Math.floor(Math.random() * 5);
-            square.style.backgroundColor = colors[colorChoice];
+            square.style.backgroundColor = 'black';
         });
+        grid.appendChild(square);
+    }    
+}
+
+function createRainbowGrid() {
+    for (let i = 0; i < userChoice ** 2; i++) {
+        const square = document.createElement("div");
+        setSquareStyle(square);
+        changeColor(square);
+        grid.appendChild(square);
     }
-    
-});
+}    
 
+function changeColor(square) {
+    square.addEventListener('mouseover', () => {
+        let colors = ["red", "blue", "orange", "purple", "green", "yellow"];
+        setRandomColor(colors, square);
+    });
+}
 
+function setRandomColor(colors, square) {
+    currentIndex = colors.indexOf(square.style.backgroundColor);
+    colorsMinusCurrent = colors.splice(currentIndex, 1);
+    let colorChoice = Math.floor(Math.random() * colors.length);
+    square.style.backgroundColor = colors[colorChoice];
+}
+
+function setSquareStyle(square) {
+    square.classList.add('square');
+    square.style.height = `${squareSize}px`;
+    square.style.width = `${squareSize}px`;
+}
 
